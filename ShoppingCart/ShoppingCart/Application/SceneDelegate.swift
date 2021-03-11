64ab8b0e067f17b53f68be8dc6a6653ee59cb07b
Kyeore Heo <91kyoheo@gmail.com>
2021-03-11 15:04:30 -0500

    fisrt Commit

---
+++ ShoppingCart/ShoppingCart/Application/SceneDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  SceneDelegate.swift
+//  ShoppingCart
+//
+//  Created by Kyo on 3/11/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: scene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
---
+++ ShoppingCart/ShoppingCart/Authentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingCart
+//
+//  Created by Kyo on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
