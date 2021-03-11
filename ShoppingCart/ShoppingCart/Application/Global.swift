5a8f03984d5ae689e2ae7d4ea062427d1e39522c
Kyeore Heo <91kyoheo@gmail.com>
2021-03-11 18:04:43 -0500

    applied global values

---
+++ ShoppingCart/ShoppingCart/Application/Global.swift
@@ -0,0 +1,12 @@
+//
+//  Global.swift
+//  ShoppingCart
+//
+//  Created by Kyo on 3/11/21.
+//
+
+import UIKit
+
+var ratio: CGFloat = 0.0
+var frameWidth: CGFloat = 0.0
+var frameHeight: CGFloat = 0.0
