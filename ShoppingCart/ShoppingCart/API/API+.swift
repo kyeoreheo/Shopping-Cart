fdde49f06d94d8a91a2f9b97f50694da251246e7
Kyeore Heo <91kyoheo@gmail.com>
2021-03-13 18:01:10 -0500

    added signUp and LogIn

---
+++ ShoppingCart/ShoppingCart/API/API+.swift
@@ -0,0 +1,46 @@
+//
+//  API+.swift
+//  ShoppingCart
+//
+//  Created by Kyo on 3/13/21.
+//
+import Alamofire
+
+extension API {
+    func registerUesr(name: String, email: String, mobile: String,
+                      password: String, completion: @escaping(String?) -> Void) {
+        let body: [String : Any] = [
+            "name": name,
+            "email": email,
+            "mobile": mobile,
+            "password": password,
+        ]
+        Alamofire.request(baseURL + register, method: .get, parameters: body)
+        .responseString { response in
+            switch response.result {
+            case .success:
+                completion(response.result.description)
+            case .failure(_):
+                completion(nil)
+            }
+            
+        }
+    }
+    
+    func logIn(mobile: String, password: String, completion: @escaping(String?) -> Void) {
+        let body: [String : Any] = [
+            "mobile": mobile,
+            "password": password,
+        ]
+        Alamofire.request(baseURL + logIn, method: .get, parameters: body)
+        .responseString { response in
+            switch response.result {
+            case .success:
+                completion(response.result.description)
+            case .failure(_):
+                completion(nil)
+            }
+            
+        }
+    }
+}
---
+++ ShoppingCart/ShoppingCart/API/API.swift
@@ -0,0 +1,26 @@
+//
+//  API.swift
+//  ShoppingCart
+//
+//  Created by Kyo on 3/13/21.
+//
+
+import Foundation
+import Alamofire
+
+class API {
+    static let shared = API()
+    private init() { }
+    
+    let baseURL = "http://rjtmobile.com/aamir/e-commerce/ios-app/"
+    let register = "shop_reg.php?"
+    let logIn = "shop_login.php?"
+    let forget = "forgot_pass_email.php?"
+    let editProfile = "edit_profile.php?"
+    let resetPassword = "shop_reset_pass.php?"
+    let topSeller = "shop_top_sellers.php?"
+    let category = "cust_category.php?"
+    let subCategory = "cust_sub_category.php?"
+    let product = "product_details.php?"
+
+}
